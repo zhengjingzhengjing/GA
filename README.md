35651c7295d5644d9d16ace995a13a0afee23059
yijingjing <1058285396@qq.com>
2020-04-05 21:52:43 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# JingBook
-This is Jing Book
+# vuepress-theme-reco-demo
+
+Blog demo for vuepress-theme-reco
+
+```bash
+# 编译
+npm run build
+
+# 启动
+npm run dev
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  "title": "JingHouse",
+  "description": "",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    valineConfig: {
+      appId: 'dL7bNHi2mwTwRObwFG4Ft0GD-gzGzoHsz',
+      appKey: 'jfD7lJ03yuq0iDNhKJWN6rad'
+    },
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时光轴",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "联系我",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "gitee",
+            "link": "https://gitee.com/yiJingJingLove",
+            "icon": "reco-npm"
+          },
+          {
+            "text": "GitHub",
+            "link": "https://github.com/zhengjingzhengjing",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "Gitee Pages",
+        "desc": "gitee pages",
+        "email": "1156743527@qq.com",
+        "link": "http://yijingjinglove.gitee.io/yjj/"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "Jing",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2020-04-05"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
